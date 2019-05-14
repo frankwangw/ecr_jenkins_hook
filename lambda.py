ff4ade1529ff3226ac6a4440fcf063e613d89550
frankwang <frank.wang@sensis.com.au>
2019-05-14 11:55:05 +1000

    clean the repo

--- lambda.py
+++
@@ -1,8 +0,0 @@
-import jenkins
-
-
-def lambda_handler(event, context):
-    # TODO implement
-    server = jenkins.Jenkins(JenkinsHostUrl, username='admin', password='myyellow')
-    
-    server.build_job(JenkinsJobName)
